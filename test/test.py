92ff84c0242d0ff1ae1954c7f97fecbe822ae64b
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-04-03 11:44:41 +0300

    добавил файл с необходимыми библиотекамии

--- test/test.py
+++ test/test.py
@@ -1,23 +1,16 @@
 import requests
 
 
-
-url = "http://127.0.0.1:5000/api/jobs/add"  # убедитесь, что URL корректен
-data = {
-    "apikey": "uCMj0DM09PUm_H0Sw1Xh3Q",
-    "job_title": "Исследование космоса",
-    "team_lead_id": 1,
-    "work_size": 200,
-    "collaborators": "1223",
-    "finish": False,
-    "email": "magomed@mail.ru"
-}
+url = "http://127.0.0.1:5000/api/jobs"
 
 
-# Для тела запроса (рекомендуется для POST):
-rsp = requests.post(url, params=data)
-print(rsp.status_code)
+response_1 = requests.get(f"{url}")
+if response_1.status_code == 200:
+    print("Запрос на получение всех пользователей выполнен успешно. TEST1")
 
 
-rsp_2 = requests.post(f"{url}?apikey=uCMj0DM09PUm_H0Sw1Xh3Q&job_title=Asist&team_lead_id=12&work_size=200&collaborators=123&finish=True&email=magomed@mail.ru")
-print(rsp_2.json())
+response_2 = requests.get(f"{url}/3")
+if response_2.status_code == 200 and response_2.json()["id"] == 3:
+    print("Запрос выполнен корректно TEST2")
+else:
+    print("Неверный id иои запрос  ", response_2.status_code)
